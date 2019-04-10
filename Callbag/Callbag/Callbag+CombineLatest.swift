731b787e8e43a79fb69f6c3c3fe9edd8fb16bdb9
chuthin <thincv@live.com>
2019-04-10 10:19:50 +0700

    Update CombineLastest

--- Callbag/Callbag/Callbag+CombineLatest.swift
+++ Callbag/Callbag/Callbag+CombineLatest.swift
@@ -8,139 +8,34 @@
 
 import Foundation
 
-public func combineLastest<L,R,O> ( _ left:@escaping Callbag<L>, _ right:@escaping Callbag<R>) -> (@escaping (L,R) -> O) -> Callbag<O> {
-    return { fn in
-        return { payload in
-            var lastL:L?
-            var lastR:R?
-            if case .start(let tb) = payload  {
-                left(.start({ payloadLeft in
-                    if case .data(let dataLeft) = payloadLeft {
-                        lastL = dataLeft
-                        if let l = lastL, let r = lastR {
-                            tb(.data(fn(l,r)))
-                        }
+public func combineLastest<L,R,O> ( _ left:@escaping Callbag<L>, _ right:@escaping Callbag<R>, _ fn:@escaping (L,R) -> O)  -> Callbag<O> {
+    return { payload in
+        var lastL:L?
+        var lastR:R?
+        if case .start(let tb) = payload  {
+            left(.start({ payloadLeft in
+                if case .data(let dataLeft) = payloadLeft {
+                    lastL = dataLeft
+                    if let l = lastL, let r = lastR {
+                        tb(.data(fn(l,r)))
                     }
-                    else if case .end = payloadLeft{
-                        tb(.end)
+                }
+                else if case .end = payloadLeft{
+                    tb(.end)
+                }
+            }))
+            
+            right(.start({ payloadRight in
+                if case .data(let dataRight) = payloadRight {
+                    lastR = dataRight
+                    if let l = lastL, let r = lastR {
+                        tb(.data(fn(l,r)))
                     }
-                }))
-                
-                right(.start({ payloadRight in
-                    if case .data(let dataRight) = payloadRight {
-                        lastR = dataRight
-                        if let l = lastL, let r = lastR {
-                             tb(.data(fn(l,r)))
-                        }
-                    }
-                    else if case .end = payloadRight{
-                        tb(.end)
-                    }
-                }))
-            }
+                }
+                else if case .end = payloadRight{
+                    tb(.end)
+                }
+            }))
         }
     }
 }
-
-/*
-extension Callbag where T == Any {
-    public static func combineLastest<T1,T2,T>(_ callbag1:Callbag<T1>,_ callbag2:Callbag<T2>,fn:@escaping(T1,T2) -> T) -> Callbag<T> {
-        let source1:Source<T1> = callbag1.source
-        let source2:Source<T2> = callbag2.source
-        var lastA:T1?
-        var lastB:T2?
-        
-        let out:Source<T> = { talkback in
-            source1({ payload in
-                switch(payload){
-                case .Start(let tb):
-                    talkback(SinkPayload.Start(tb))
-                case .Push(let v):
-                    lastA = v
-                    if let a = lastA,let b = lastB
-                    {
-                        talkback(SinkPayload.Push(fn(a,b)))
-                    }
-                case .End:
-                    talkback(SinkPayload.End)
-                }
-            })
-            
-            source2({ payload in
-                switch(payload){
-                case .Start(let tb):
-                    talkback(SinkPayload.Start(tb))
-                case .Push(let v):
-                    lastB = v
-                    if let a = lastA,let b = lastB
-                    {
-                        talkback(SinkPayload.Push(fn(a,b)))
-                    }
-                case .End:
-                    talkback(SinkPayload.End)
-                }
-            })
-        }
-        
-        return Callbag<T>(source:out)
-    }
-    
-    public static func combineLastest<T1,T2,T3,T>(callbag1:Callbag<T1>,callbag2:Callbag<T2>,callbag3:Callbag<T3>,fn:@escaping(T1,T2,T3) -> T) -> Callbag<T> {
-        let source1:Source<T1> = callbag1.source
-        let source2:Source<T2> = callbag2.source
-        let source3:Source<T3> = callbag3.source
-        var last1:T1?
-        var last2:T2?
-        var last3:T3?
-        
-        let out:Source<T> = { talkback in
-            source1({ payload in
-                switch(payload){
-                case .Start(let tb):
-                    talkback(SinkPayload.Start(tb))
-                case .Push(let v):
-                    last1 = v
-                    if let l1 = last1,let l2 = last2 , let l3 = last3
-                    {
-                        talkback(SinkPayload.Push(fn(l1,l2,l3)))
-                    }
-                case .End:
-                    talkback(SinkPayload.End)
-                }
-            })
-            
-            source2({ payload in
-                switch(payload){
-                case .Start(let tb):
-                    talkback(SinkPayload.Start(tb))
-                case .Push(let v):
-                    last2 = v
-                    if let l1 = last1,let l2 = last2 , let l3 = last3
-                    {
-                        talkback(SinkPayload.Push(fn(l1,l2,l3)))
-                    }
-                case .End:
-                    talkback(SinkPayload.End)
-                }
-            })
-            
-            source3({ payload in
-                switch(payload){
-                case .Start(let tb):
-                    talkback(SinkPayload.Start(tb))
-                case .Push(let v):
-                    last3 = v
-                    if let l1 = last1,let l2 = last2 , let l3 = last3
-                    {
-                        talkback(SinkPayload.Push(fn(l1,l2,l3)))
-                    }
-                case .End:
-                    talkback(SinkPayload.End)
-                }
-            })
-        }
-        
-        return Callbag<T>(source:out)
-    }
-}
-*/
